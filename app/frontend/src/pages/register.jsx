8a285b847ef7d88f5c9dead2d37e42ee6169ba88
Kalana-Bimsara <132051412+Kalana-Bimsara@users.noreply.github.com>
2025-11-30 23:54:42 +0530

    Update register.jsx

--- app/frontend/src/pages/register.jsx
+++ app/frontend/src/pages/register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
   } = useForm();
 
   const BACKEND_PORT = import.meta.env.BACKEND_PORT || "9000";
-  const HOST = import.meta.env.HOST || "http://localhost";
+  const HOST = import.meta.env.HOST || "http://13.203.72.117";
 
   const onSubmit = async (data) => {
     try {
