2630de71a1211f2a4a07b2f7cbbf470c5e80afcd
Kalana-Bimsara <132051412+Kalana-Bimsara@users.noreply.github.com>
2026-03-02 15:27:03 +0530

    fixed bugs

--- app/frontend/src/components/navbar.jsx
+++ app/frontend/src/components/navbar.jsx
@@ -109,12 +109,7 @@ const NavbarComponent = () => {
               </li>
             </ul>
             {/* Book Now Button */}
-            <button
-              className="btn btn-primary btn-lg ms-3"
-              onClick={() => console.log("⚠️ Injected F14 – Booking disabled")}
-            >
-              Book Now
-            </button>
+            <Link to="/booking" className="btn btn-primary btn-lg ms-3" >Book Now</Link>
             {/* <Link to="/login" className="btn btn-success btn-lg ms-3" >Log In</Link> */}
             {/* Show Log In only when NOT logged in */}
             {!isLoggedIn && (
--- app/frontend/src/components/prices.jsx
+++ app/frontend/src/components/prices.jsx
@@ -1,10 +1,6 @@
 // React import not required with the new JSX runtime
 
 const Prices = () => {
-
-  console.log("⚠️ Injected Fault F13 – Prices page blank");
-  return null;
-
   return (
     <>
       <section id="prices" className="py-5 --bs-primary">
